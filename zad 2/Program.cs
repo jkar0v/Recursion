cf64ee2bf7937a30c7486e98e856145bc382c153
Nikito69 <nikiniki2777@gmail.com>
2025-02-14 15:18:54 +0200

    Add project files.

---
+++ zad 2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace zad_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{Powerof2(number)}");
+        }
+        static bool Powerof2(int number)
+        {
+            if (number == 1)
+            {
+                return true;
+            }
+            if (number % 2 == 0)
+            {
+                return Powerof2(number / 2);
+            }
+            return false;
+        }
+    }
+}
