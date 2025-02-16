329e7e523bd7844e8ed516b0f50d88942fd0b4e8
Jivko Karov <jikito2007@gmail.com>
2025-02-16 14:01:44 +0200

    Added zad 1 and zad 3. Completed already

---
+++ zad 3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace zad_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's reverse the number:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Reversed:");
+            Console.WriteLine(Reversed(n));
+        }
+        static int Reversed(int number, int reversed = 0)
+        {
+            if (number == 0)
+            {
+                return reversed;
+            }
+            return Reversed(number / 10, reversed * 10 + number % 10);
+        }
+    }
+}
---
+++ zad1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to be calculated:");
+            long number = long.Parse(Console.ReadLine());
+            Console.WriteLine(Calculate(number));
+        }
+        static long Calculate(long number)
+        {
+            if(number == 0)
+            {
+                return 0;
+            }
+            return (number % 10) + Calculate(number / 10);
+        }
+    }
+}
